2a416bed692bccfc7759a19b9eb2d13fbf9fc96d
zpei100 <zhengqing.pei@gmail.com>
2018-08-15 21:15:52 -0400

    Added tests for API call

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,10 +3,13 @@ const request = require('supertest');
 
 const {app} = require('../server.js');
 const {Todo} = require('../models/todo.js');
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+	_id: new ObjectID(), 
 	text: 'First test todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo'
 }];
 
@@ -67,14 +70,48 @@ describe('post /todos', () => {
 
 
 //test is failing: cannot get route: /todos
-describe('Get /todos', () => {
-	it('should get all todos', (done) => {
+// describe('Get /todos', () => {
+// 	it('should get all todos', (done) => {
+// 		request(app)
+// 			.get('/todos')
+// 			.expect(200)
+// 			.expect((res) => {
+// 				expect(res.body.todos.length).toBe(2);
+// 			})
+// 			.end(done);		
+// 	})
+// });
+
+describe('Get /todos/:id', () => {
+	it('should return todo doc', (done) => {
 		request(app)
-			.get('/todos')
+			.get(`/todos/${todos[0]._id.toString()}`)
 			.expect(200)
 			.expect((res) => {
-				expect(res.body.todos.length).toBe(2);
+				expect(res.body.text).toBe(todos[0].text);
 			})
-			.end(done);		
-	})
+			.end(done);
+	});
+
+	it('should return a 404, if todo not found', (done) => {
+		var ID = new ObjectID().toString();
+		request(app)
+			.get('/todos/${ID}')
+			.expect(404)
+			.end(done);
+		//valid ID but does not exist in our data base;
+
+		//make sure we get a 404 back;
+	});
+
+	it('should return a 404, if ID provided is not objectID', (done) => {
+		request(app)
+			.get(`/todos/123`)
+			.expect(404)
+			.end(done);
+		// /todos/123 to pass in as URL;
+
+
+
+	});
 });
